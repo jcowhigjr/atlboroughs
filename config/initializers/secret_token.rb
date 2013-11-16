d7b83df304ba38e3a9cadfed3253f83dc0ddaf75
John <jcowhigjr+dev@gmail.com>
2013-11-16 00:05:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atlboroughs::Application.config.secret_token = '0e4013a2a7150a6a0f217857b842b2e0fd9c6da63804547417beab9a9fbd56f75a5d134873d8d4848a56b6cced6185a56fb3281ab6f5719c449ff8f2e06a6c84'
